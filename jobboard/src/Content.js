f26cf0cfb98f0baabd502499bf424b08566b0ce0
XCannere <xavier.cannere@epitech.eu>
2023-10-10 11:36:50 +0200

    step 3 htmlcss

--- jobboard/src/Content.js
+++ jobboard/src/Content.js
@@ -1,11 +1,21 @@
 import './App.css';
+import React, { useState } from 'react';
+import Offer from './Offer.js';
 
 function Content(props) {
+
+    const [displayOffer, setDisplayOffer] = useState(false);
+
+    const handleClick = () => {
+        setDisplayOffer(!displayOffer);
+    };
+
   return (
     <div className="Content">
         <h1>{props.title}</h1>
         <p>{props.text}</p>
-        <button>Learn more</button>
+        <button onClick={handleClick}>Learn more</button>
+        {displayOffer && <Offer fermer={() => setDisplayOffer(false)} />}
     </div>
   );
 }
---
+++ jobboard/src/Offer.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Offer({fermer}) {
+
+  return (
+    <div className="Offer">
+      <button onClick={fermer}>Back</button>
+    </div>
+  );
+}
+
+export default Offer;
