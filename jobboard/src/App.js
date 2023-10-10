8668aeb49e5b0337f034e7d58cc672e67ad698d7
XCannere <xavier.cannere@epitech.eu>
2023-10-10 10:28:56 +0200

    step 2 htmlcss

--- jobboard/src/App.js
+++ jobboard/src/App.js
@@ -4,14 +4,14 @@ import Content from './Content.js';
 function App() {
   return (
     <div className="App">
-        <Content title="Job 1"/>
-        <Content title="Job 2"/>
-        <Content title="Job 3"/>
-        <Content title="Job 4"/>
-        <Content title="Job 5"/>
-        <Content title="Job 6"/>
-        <Content title="Job 7"/>
-        <Content title="Job 8"/>
+        <Content title="Job 1" text="Ceci est la première annonce"/>
+        <Content title="Job 2" text="Ceci est la deuxième annonce"/>
+        <Content title="Job 3" text="Ceci est la troisième annonce"/>
+        <Content title="Job 4" text="Ceci est la quatrième annonce"/>
+        <Content title="Job 5" text="Ceci est la cinquième annonce"/>
+        <Content title="Job 6" text="Ceci est la sixième annonce"/>
+        <Content title="Job 7" text="Ceci est la septième annonce"/>
+        <Content title="Job 8" text="Ceci est la huitième annonce"/>
     </div>
   );
 }
--- jobboard/src/Content.js
+++ jobboard/src/Content.js
@@ -4,6 +4,8 @@ function Content(props) {
   return (
     <div className="Content">
         <h1>{props.title}</h1>
+        <p>{props.text}</p>
+        <button>Learn more</button>
     </div>
   );
 }
