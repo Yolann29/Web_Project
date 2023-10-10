2df1651f7c18d2f9aad18c15894515f6ec6c6ec9
XCannere <xavier.cannere@epitech.eu>
2023-10-10 10:19:35 +0200

    maj htmlcss

--- jobboard/src/App.js
+++ jobboard/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from './Content.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+        <Content title="Job 1"/>
+        <Content title="Job 2"/>
+        <Content title="Job 3"/>
+        <Content title="Job 4"/>
+        <Content title="Job 5"/>
+        <Content title="Job 6"/>
+        <Content title="Job 7"/>
+        <Content title="Job 8"/>
     </div>
   );
 }
---
+++ jobboard/src/Content.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Content(props) {
+  return (
+    <div className="Content">
+        <h1>{props.title}</h1>
+    </div>
+  );
+}
+
+export default Content;
