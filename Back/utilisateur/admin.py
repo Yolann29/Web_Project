bd3b412c92fc6368b893ff53c07321f9ad13f155
XCannere <xavier.cannere@epitech.eu>
2023-10-11 10:57:31 +0200

    initialisation django

---
+++ Back/utilisateur/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from utilisateur.models import cmp
+
+admin.site.register(cmp)
---
+++ Back/utilisateur/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class cmp(models.Model):
+    surname = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=30)
+    password = models.CharField()
+    email = models.EmailField()
