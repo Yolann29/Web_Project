429e1447a5bee18f22fe526f1431e5bc3efe3346
Yolann29 <victor.morellet@epitech.eu>
2023-10-13 17:00:22 +0200

    Merge

--- Back/utilisateur/admin.py
+++ Back/utilisateur/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from utilisateur.models import cmp, permissions, companies, advertisement
 
 admin.site.register(cmp)
+admin.site.register(permissions)
+admin.site.register(companies)
+admin.site.register(advertisement)
