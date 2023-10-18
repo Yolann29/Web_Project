2308bdf5172009e9ce1f992fdc861a7ba52d2c3d
XCannere <xavier.cannere@epitech.eu>
2023-10-18 14:32:05 +0200

    recuperation

--- jobboard/src/Connexion.js
+++ jobboard/src/Connexion.js
@@ -1,12 +1,11 @@
-function Connexion({connect, connect2, connect3}) {
+function Connexion({connect, connect2}) {
 
     return (
       <div className="Connexion">
             <h1>Welcome to Jobboard</h1>
             <div>
                 <button onClick={connect}>Sign in</button>
-                <button onClick={connect2}>User register</button>
-                <button onClick={connect3}>Admin register</button>
+                <button onClick={connect2}>Register</button>
             </div>
       </div>
     );
